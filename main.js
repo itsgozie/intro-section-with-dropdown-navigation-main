0e27f3edb40e83bbc203254533af5141b1ea9a61
Gozie <87874051+itsgozie@users.noreply.github.com>
2023-10-04 17:12:08 +0100

    Structure and Styles update

---
+++ main.js
@@ -0,0 +1,11 @@
+// const list = document.querySelector("ul.user-access").children[1];
+
+// const btn = document.createElement('button');
+// btn.className = 'btn btn-nav';
+// btn.innerHTML = '<a></a>'
+// btn.children[0].textContent = 'Register';
+// list.appendChild(btn); 
+
+
+const brands = document.querySelector('div.brands');
+console.log(brands.childNodes);
