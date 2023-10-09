8b1529ea151b580d2e07a1030169eb455cb3c5ea
Gozie <87874051+itsgozie@users.noreply.github.com>
2023-10-10 00:49:18 +0100

    app update

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-// const list = document.querySelector("ul.user-access").children[1];
-
-// const btn = document.createElement('button');
-// btn.className = 'btn btn-nav';
-// btn.innerHTML = '<a></a>'
-// btn.children[0].textContent = 'Register';
-// list.appendChild(btn); 
-
-
-const brands = document.querySelector('div.brands');
-console.log(brands.childNodes);
+const app = Vue.createApp({
+    data() {
+        return {
+            heading: 'Make remote work alive'
+        };
+    }
+}).mount('#heading');
