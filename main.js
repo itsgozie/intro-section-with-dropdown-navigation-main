53b8c211626e175ad480f0d597fd91afd43ef9ed
Gozie <87874051+itsgozie@users.noreply.github.com>
2023-10-21 17:46:27 +0100

    app update

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 const app = Vue.createApp({
     data() {
         return {
-            heading: 'Make remote work alive'
+            heading: 'Make remote work alive',
+            links: ['Home', 'About', 'Pricing', 'Plan', 'Contants'],
+            firstName: 'Emma',
+            LastName: 'May',
+            url: [
+                {
+                    home: 'index.html',
+                    about: 'about.html',
+                    pricing: 'pricing.html',
+                    plan: 'plan.html',
+                    contact: 'contact.html'
+                }
+            ],
+            urlName: ['Home', 'About', 'Pricing', 'Plan', 'Contact']
         };
+    },
+    methods: {
+        heroHeading() {
+            return 'Make remote work'
+        }
     }
-}).mount('#heading');
+}).mount('body');
+
+document.getElementById('btn').addEventListener('click', onclick);
+
+function onclick() {
+    console.log('CTA clicked');
+}
+
